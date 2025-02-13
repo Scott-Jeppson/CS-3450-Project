f056c1cdbf82b21c65fca6f171701f6a13e24faa
Natalia Lewis <a02296603@usu.edu>
2025-02-12 21:24:27 -0700

    Set up React router with only the Home route for now

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from './Pages/Home.jsx';
+
+// Main component of the app where the routing and global state management is defined
+function App() {
+
+    // Future: Global states managed here such as user state
+
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    </BrowserRouter>
+  );
+}
