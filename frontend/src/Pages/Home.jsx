400021ef8398eae000e544dce09ede520f8d1215
Natalia Lewis <a02296603@usu.edu>
2025-02-16 22:56:40 -0700

    Implemented greeting message, added visual transformations for accessibility, and fixed various bugs

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,18 +1,37 @@
-import React, { useEffect, useState } from "react";
-import traffic_pic from "../traffic_pic.jpg";
+import { useEffect, useState } from "react";
+import { useNavigate } from 'react-router-dom';
 import Navbar from "../components/navbar.jsx";
 import './home.css';
 
 const Home = () => {
+    const navigate = useNavigate();
+
+    const handleSignInClick = () => {
+        navigate('/signin');
+    };
+
+    const handleCreateAccountClick = () => {
+        navigate('/createaccount');
+    };
 
     return (
         <div className="page-div" id="home-page">
             <Navbar/>
-            <div>
-                <img src={traffic_pic} id="traffic-pic"></img>
+            <div id="main-content">
+                <div id="welcome-box">
+                    <h1>Welcome to StreamLine!</h1>
+                    <p> StreamLine is a traffic simulation and optimization tool designed to help traffic managers in
+                        Utah County analyze and improve traffic flow.
+                    </p>
+                    <div className="divider">
+                      <h4>Please sign in to continue:</h4>
+                    </div>
+                    <button className="home-button" onClick={handleSignInClick}>Sign In</button>
+                    <button className="home-button" onClick={handleCreateAccountClick}>Create Account</button>
+                </div>
             </div>
         </div>
     );
-};
+}
 
 export default Home;
