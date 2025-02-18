96288d3e13e081acc543a017342535074a77207b
Natalia Lewis <a02296603@usu.edu>
2025-02-17 22:10:20 -0700

    Added backend connection route under /test

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './Pages/Home.jsx';
 import SignIn from './Pages/SignIn.jsx';
 import CreateAccount from './Pages/CreateAccount.jsx';
+import Test from './Pages/Test.jsx';
 
 // Main component of the app where the routing and global state management is defined
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <Route index element={<Home />} />
             <Route path="/signin" element={<SignIn />} />
             <Route path="/createaccount" element={<CreateAccount />} />
+            <Route path="/test" element={<Test />} />
         </Routes>
     </BrowserRouter>
   );
