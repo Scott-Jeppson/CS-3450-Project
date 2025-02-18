80bffa11258361fe26fa4ac6fb943ed7af286dba
Natalia Lewis <a02296603@usu.edu>
2025-02-17 23:23:14 -0700

    Implemented state changes for navbar based on if a user is logged in. Also implemented the log out function.

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,19 +1,32 @@
 import './navbar.css';
 import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
 
-function Navbar() {
+function Navbar({ isLoggedIn, setIsLoggedIn }) {
+
+    const handleLogout = () => {
+        // Clear the login token from localStorage and set isLoggedIn to false
+        localStorage.removeItem('loginToken');
+        setIsLoggedIn(false);
+    };
 
     return (
-        <nav id='navbar'>
+        <nav id={isLoggedIn ? "navbar-logged-in" : "navbar"}>
             <div id="nav-brand">
                 <Link to="/">
-                    <img id="logo" src="src/assets/Logo-Light-Purple-Circle.svg" />
+                    <img id="logo" src={isLoggedIn ? "src/assets/Logo-White-Text-Purple-Background.svg" :
+                        "src/assets/Logo-Light-Purple-Circle.svg"} />
                 </Link>
                 <Link to="/" id="nav-name">
                     <h2>StreamLine</h2>
                 </Link>
             </div>
-            <Link to="/SignIn" id="nav-button">Sign In</Link>
+            {isLoggedIn ? (
+                    <Link to="/" id="nav-button" onClick={handleLogout}>Log Out</Link>
+                ) : (
+                    <Link to="/SignIn" id="nav-button">Sign In</Link>
+                )
+            }
         </nav>
     );
 }
