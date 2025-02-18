75d33b737984eeb59c94f634a140056ae786b298
Natalia Lewis <a02296603@usu.edu>
2025-02-17 22:10:34 -0700

    Added logo to navbar

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,27 +1,20 @@
 import './navbar.css';
-import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
-import { getHelloWorld } from "../api";
 
 function Navbar() {
-    const [testMessage, setTestMessage] = useState("");
-
-    {/* Kept for now to show that the backend is connected*/}
-    useEffect(() => {
-            async function fetchMessage() {
-                const testData = await getHelloWorld();
-                setTestMessage(testData.message);
-            }
-            fetchMessage();
-        }, []);
 
     return (
-        <div id='navbar'>
-            <Link to="/" id="nav-brand"><h2>StreamLine</h2></Link>
-            {/* Kept for now to show that the backend is connected*/}
-            <h4>Backend Response: {testMessage}</h4>
+        <nav id='navbar'>
+            <div id="nav-brand">
+                <Link to="/">
+                    <img id="logo" src="src/assets/Logo-Light-Purple-Circle.svg" />
+                </Link>
+                <Link to="/" id="nav-name">
+                    <h2>StreamLine</h2>
+                </Link>
+            </div>
             <Link to="/SignIn" id="nav-button">Sign In</Link>
-        </div>
+        </nav>
     );
 }
 
