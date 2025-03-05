e53319ac8df449b6e2b9a81875712f3861866a6a
Ally Hassett <allyhassett@gmail.com>
2025-03-05 16:14:11 -0700

    Made toolbar collapsible

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -8,34 +8,44 @@ import "./dashboard.css";
 const Dashboard = ({ isLoggedIn, setIsLoggedIn }) => {
     const navigate = useNavigate();
 
+    // const [isLoggedIn, setIsLoggedIn] = useState(true); // Example logged-in state
+    const [isToolbarCollapsed, setIsToolbarCollapsed] = useState(false); // State for toolbar collapse
+
+    const toggleToolbar = () => {
+        setIsToolbarCollapsed(!isToolbarCollapsed); // Toggle the toolbar state
+    };
+
     return (
-        <div className="page-div" style={{ backgroundColor: "var(--grey)"}}>
+        <div className="page-div" style={{ backgroundColor: "var(--grey)", overflowY: "auto" }}>
             <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
             
             
 
             <div id="main-content">
                 {isLoggedIn ? (
-                        // <>
-                        //     {/* <Toolbar /> */}
-                        //     {/* <SumoSim /> */}
-                        // </>
                         <div className="dashboard-screen">
                             {/* <div className="toolbar"></div> */}
-                            <Toolbar />
+                            {/* <Toolbar /> */}
+                            <div className={`toolbar ${isToolbarCollapsed ? 'toolbar-collapsed' : ''}`} style={{ alignItems: "right" }}>
+                                <button className="toggle-btn" onClick={toggleToolbar}>
+                                    ☰
+                                </button>
+                            </div>
                             <div className="info-screen">
                                 <div className="stats">
                                     <div className="stats-items">10%</div>
                                     <div className="stats-items">20%</div>
                                     <div className="stats-items">30%</div>
+                                    <div className="stats-items">40%</div>
                                 </div>
                                 <div className="tools">
                                     <SumoSim />
-                                    <div className="tools-items">Tool</div>
+                                    <div className="tools-items">Tool 1</div>
+                                    <div className="tools-items">Tool 2</div>
                                 </div>
                                 <div className="tools">
                                     <SumoSim />
-                                    <div className="tools-items">Tool</div>
+                                    <div className="tools-items">Tool 3</div>
                                 </div>
                             </div>
                         </div>
