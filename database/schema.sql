7ee1a6db3a831940b6064d1b20a2e8753870a554
Scott Jeppson <sfjepp46@gmail.com>
2025-03-05 13:36:26 -0700

    Fixed error where database and/or user_information table are not created when composing up

--- database/schema.sql
+++ database/schema.sql
@@ -1,8 +1,22 @@
 -- Create the database user and assign a password
-CREATE USER myuser WITH PASSWORD 'mypassword';
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'myuser') THEN
+      CREATE ROLE myuser WITH LOGIN PASSWORD 'mypassword';
+   END IF;
+END
+$do$;
 
 -- Create the database (if not already created)
-CREATE DATABASE mydatabase;
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'mydatabase') THEN
+      CREATE DATABASE mydatabase;
+   END IF;
+END
+$do$;
 
 -- Grant all privileges on the database to the user
 GRANT ALL PRIVILEGES ON DATABASE mydatabase TO myuser;
