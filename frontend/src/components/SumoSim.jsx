060509b7d4851116268bdee68f4986e05632c756
Natalia Lewis <a02296603@usu.edu>
2025-03-20 16:02:02 -0600

    Changed name of the header

--- frontend/src/components/SumoSim.jsx
+++ frontend/src/components/SumoSim.jsx
@@ -4,7 +4,7 @@ const SimulationMap = () => {
 
   return (
     <div id="sumo-sim" style={{ width: "100%", height: "100%", overflow: "hidden" }}>
-      <h1>SUMO Traffic Simulation</h1>
+      <h1>Traffic Simulation</h1>
       <iframe
         src="http://localhost:5000/"
         width="100%"
