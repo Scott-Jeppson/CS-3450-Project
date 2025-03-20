66078a3492a528e9ca05f1f4d2e5d765257ff0b8
Natalia Lewis <a02296603@usu.edu>
2025-03-20 16:06:00 -0600

    Simplified dashboard page by making the toolbar a component

--- frontend/src/Pages/Dashboard.jsx
+++ frontend/src/Pages/Dashboard.jsx
@@ -6,51 +6,32 @@ import Toolbar from "../components/toolbar.jsx";
 import "./dashboard.css";
 
 const Dashboard = ({ isLoggedIn, setIsLoggedIn }) => {
-//     const navigate = useNavigate();
-//
-//     // const [isLoggedIn, setIsLoggedIn] = useState(true); // Example logged-in state
-//     const [isToolbarCollapsed, setIsToolbarCollapsed] = useState(false); // State for toolbar collapse
-//
-//     const toggleToolbar = () => {
-//         setIsToolbarCollapsed(!isToolbarCollapsed); // Toggle the toolbar state
-//     };
 
     return (
         <div className="page-div" style={{ backgroundColor: "var(--grey)", overflowY: "auto" }}>
             <Navbar isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-            
-            
 
-            <div id="main-content">
+            <div id="main-content-dashboard">
                 {isLoggedIn ? (
-                        <div className="dashboard-screen">
+                    <>
+                        <Toolbar />
 
-                            <Toolbar />
-{/*                             <div className={`toolbar ${isToolbarCollapsed ? 'toolbar-collapsed' : ''}`} style={{ alignItems: "right" }}> */}
-{/*                                 <button className="toggle-btn" onClick={toggleToolbar}> */}
-{/*                                     ☰ */}
-{/*                                 </button> */}
-{/*                             </div> */}
-
-                            {/* Dashboard Cards */}
-                            <div className="info-screen">
-                                <div className="stats">
-                                    {/* <div className="stats-items">10%</div>
-                                    <div className="stats-items">20%</div>
-                                    <div className="stats-items">30%</div>
-                                    <div className="stats-items">40%</div> */}
-                                </div>
-                                <div className="tools">
-                                    <SumoSim />
-                                    {/* <div className="tools-items">Tool 1</div>
-                                    <div className="tools-items">Tool 2</div> */}
-                                </div>
-                                {/* <div className="tools">
-                                    <SumoSim />
-                                    <div className="tools-items">Tool 3</div>
-                                </div> */}
-                            </div>
+                        <div className="sumo-sim">
+                            <SumoSim />
                         </div>
+
+{/*                                 <div className="stats"> */}
+{/*                                      */}{/* <div className="stats-items">10%</div> */}
+{/*                                     <div className="stats-items">20%</div> */}
+{/*                                     <div className="stats-items">30%</div> */}
+{/*                                     <div className="stats-items">40%</div> */}
+{/*                                 </div> */}
+{/*                                 <div className="tools"> */}
+{/*                                     <SumoSim /> */}
+{/*                                     <div className="tools-items">Tool 1</div> */}
+{/*                                     <div className="tools-items">Tool 2</div> */}
+{/*                                 </div> */}
+                    </>
                     ) : (
                         <h1>You are not logged in</h1>
                     )
