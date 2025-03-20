d9ed28dd62e152b32cf88770a6b9d774f8a833f4
Natalia Lewis <a02296603@usu.edu>
2025-03-20 15:23:14 -0600

    Made it so clicking out of the navdrawer closes it, as well as going to a different page

--- frontend/src/components/toolbar.jsx
+++ frontend/src/components/toolbar.jsx
@@ -1,6 +1,6 @@
 import './toolbar.css';
 import { Link } from 'react-router-dom';
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 function Toolbar({ isLoggedIn, setIsLoggedIn }) {
 
@@ -17,11 +17,26 @@ function Toolbar({ isLoggedIn, setIsLoggedIn }) {
         setIsToolbarCollapsed(!isToolbarCollapsed);
     };
 
+    // Close the toolbar when the user clicks outside of it
+    const toolbarRef = useRef(null);
+
+    useEffect(() => {
+        const closeToolbar = (e) => {
+            if (!isToolbarCollapsed && !event.target.closest('#toggle-drawer') && !toolbarRef.current.contains(e.target)) {
+                setIsToolbarCollapsed(true);
+            }
+        };
+
+        document.addEventListener('click', closeToolbar);
+
+        return () => document.removeEventListener('click', closeToolbar);
+    }, [isToolbarCollapsed]);
+
     return (
         <>
             <button id="toggle-drawer" onClick={toggleToolbar}>☰</button>
 
-            <div className={`toolbar ${isToolbarCollapsed ? 'collapsed' : ''}`}>
+            <div className={`toolbar ${isToolbarCollapsed ? 'collapsed' : ''}`} ref={toolbarRef}>
                 <Link to="/" className="drawer-link" aria-label="Home">Home</Link>
     {/*             <Link to="/about" className="drawer-link" aria-label="About">About</Link> */}
     {/*             <Link to="/sumo" className="drawer-link" aria-label="Traffic simulation">Traffic Simulation</Link> */}
