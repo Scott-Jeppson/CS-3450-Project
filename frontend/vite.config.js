bb1ea2914f80bc7b4cb7465c807a37c1cd6e9fe6
FroostySnoowman <jacob.j.beal@icloud.com>
2025-04-16 16:44:56 -0600

    Updated Vite Config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  }
-});
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    strictPort: true,
+  },
+})
