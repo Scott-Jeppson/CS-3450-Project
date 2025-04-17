964e3f42af8c6a5e05c45ec8db8c460f7807abd4
FroostySnoowman <jacob.j.beal@icloud.com>
2025-04-17 14:29:50 -0600

    Changed 5000 to 8081

--- backend/src/dashboard.py
+++ backend/src/dashboard.py
@@ -40,10 +40,10 @@ app = cors(
     allow_origin=[
         "http://localhost:5173",
         "http://localhost:8080",
-        "http://localhost:5000",
+        "http://localhost:8081",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:8080",
-        "http://127.0.0.1:5000",
+        "http://127.0.0.1:8081",
         "http://129.123.153.13:5173",
         "https://sapi.servicesx.net",
         "https://ssapi.servicesx.net",
