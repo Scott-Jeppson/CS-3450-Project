4fe194e58d5253671558e69f5955e1c80f73d77b
FroostySnoowman <jacob.j.beal@icloud.com>
2025-04-18 13:39:09 -0600

    a

--- backend/src/dashboard.py
+++ backend/src/dashboard.py
@@ -137,5 +137,5 @@ async def startup():
 
 if __name__ == "__main__":
     config = Config()
-    config.bind = ["0.0.0.0:80"]
+    config.bind = ["0.0.0.0:8080"]
     asyncio.run(hypercorn.asyncio.serve(app, config))
